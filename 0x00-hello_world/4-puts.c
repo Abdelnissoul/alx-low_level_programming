0e6b811f8a55bfcaf38b3bdd6a96869319e34e91
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-05 07:25:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main (void){puts("\"Programming is like building a multilingual puzzle"); return 0;}
+/**
+ * implementing the function that prints instead of printf
+ *
+ * Return: 0 (success)
+ */
+int main (void){
+	puts("\"Programming is like building a multilingual puzzle");
+       	return 0;
+}
