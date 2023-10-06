637a5007e2e52c12b7dfd8dff865cc428c83dd21
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-06 12:57:03 -0700

    printing digits from base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing numbers of base 16
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char num;
+for (num = '0' ; num <= '9' ; num++)
+{
+putchat(num);
+}
+for (num = 'a' ; num <= 'f' ; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
