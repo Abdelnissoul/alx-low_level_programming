34c0d65dac93de3fcb857e3940dac6e1edb3bab1
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-10 05:49:31 -0700

    Printing alphabets in lowercase

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - printing all possible combinations of two two-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int num1;
+	for (num = 0 ; num <= 98 ; )
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Printing alphabets in lowercase.
+ */
+void print_alphabet(void)
+{
+	char l;
+	for (l = 'a' ; l<= 'z' ; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
