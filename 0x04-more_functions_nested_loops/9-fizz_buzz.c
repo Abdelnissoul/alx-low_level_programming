eb2ec775757cf57bef1a9d485605199e6656defc
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-12 17:14:28 -0700

    fizzbuzz correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,17 @@ int main(void)
 
 	for (num = 1 ; num <= 100 ; num++)
 	{
-		if (num % 15 == 0)
+		if ((num % 15) == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (num % 3 == 0)
+		else if ((num % 3) == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (num % 5 == 0)
+		else if ((num % 5) == 0)
 		{
+			if (num != 100)
 			printf("Buzz ");
 		}
 		else
