19dec96069a578a516b72afd73ebac9f24460a72
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-16 11:37:24 -0700

    generating password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,5 +23,18 @@ int main(void)
 		charset[start++] = a;
 	}
 	charset[start] = '\0';
+
+int l_password = 25;
+char password[l_password +1];
+	srand(time(NULL));
+
+	for (int i = 0; i < password_l; i++)
+	{
+		password[i] = charset[rand() % 62];
+	}
+	password[password_l] = '\0';
+	printf("the password is: %s\n", password);
+
+	return (0);
 }
 
