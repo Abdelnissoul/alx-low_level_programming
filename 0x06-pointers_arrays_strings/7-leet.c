96b0d99802324b2a447a9756a91862fc76a4331f
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-18 12:39:15 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ * @n: the pointer that will decode
+ * Return: n (Success)
+ */
+char *leet(char *n)
+{
+	char *l_letters = "aAeEoOtTlL";
+	char *l_numbers = "4433007711";
+	int a;
+	int b;
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; l_letters[b] != '\0'; b++)
+		{
+			if (n[a] == l_letters[b])
+			{
+				str[a] = l_numbers[b];
+			}
+		}
+	}
+	return (n);
+}
