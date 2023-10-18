e37d06b5e1b731652e08482f01d0a6e94ad49713
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-18 11:45:42 -0700

    verifying..

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * strcmp - comapre string values
+ * _strcmp - comapre string values
  * @s1: pointer for the first string
  * @s2: pointer for the second one
  *
  * Return: s1[v] - s2[v]
  */
-int strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int v = 0;
 
