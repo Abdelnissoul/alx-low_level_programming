8e2dd9fac39b1f6f2663bcc076c70e83437bd13a
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-23 06:19:40 -0700

    string to char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * @s: pointer to the string to be searched
  * @c: input character to be stored
  *
- * Return: (0) Success
+ * Return: (NULL) or address of s Success
  */
 char *_strchr(char *s, char c)
 {
@@ -15,5 +15,5 @@ char *_strchr(char *s, char c)
 		if (s[a] == c)
 			return (&s[a]);
 	}
-	return (0);
+	return (NULL);
 }
