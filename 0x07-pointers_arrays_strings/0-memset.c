f8b304c5d9a8e13bf0e07f87c72885a662e74b81
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-23 04:48:12 -0700

    changing array with value n

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to the memory allocated that will be filled
+ * @b: value of bytes to be written
+ * @n: number of bytes ro be changed
+ *
+ * Return: pointer to the allocated memory s after it is filled.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
