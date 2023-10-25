7546f1b8b4e709916074c03bfb182d4dfc73ba25
Abdelnissoul <Abdelnissoul205@gmail.com>
2023-10-25 06:19:56 -0700

    testing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+int _sqrt_assist(int n, int a);
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
- * _sqrt_assist - function helps keep tracking of changing variables.
  * @n: number to calculate the root
+ * @a: number for gessing the root
  * Return: the natural square root
  */
 int _sqrt_recursion(int n)
@@ -18,6 +19,11 @@ int _sqrt_recursion(int n)
 	}
 	return (_sqrt_assist(n, 1));
 }
+/***
+ * _sqrt_assist - function helps keep tracking of changing variables.
+ * a: number for tracking the square root
+ * Return: the natural square root
+ */
 int _sqrt_assist(int n, int a)
 {
 	if (a * a == n)
