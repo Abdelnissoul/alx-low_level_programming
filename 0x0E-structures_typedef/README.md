eddff1b851d2d7ca7da92cd1ffc57c868fcb97fd
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-06 12:18:59 +0000

    struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+in this tasks we will learn about struct in c programming and typedef as well as C structure packing
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a dog's attributes
+ * @name: the name of the dog
+ * @age: its age
+ * @owner: the owner
+ * Description: struct of the dog attribution
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
