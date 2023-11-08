af6696791ca69592c0eea0eb2851719f4aa7b937
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-08 13:33:50 +0000

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * main - prints opcodes
+ * @argc: arguments
+ * @argv: array arguments
+ * Return: 0 (Success) otherwise print error
+ */
+int main(int arg, char *argv[])
+{
+	int a;
+	int b;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	memcpy(&arr, &main, sizeof(main));
+
+	for (a = 0; a < b; a++)
+	{
+		if (a == b - 1)
+		{
+			printf("%02hhx\n", arr[a]);
+		}
+		else
+		{
+			printf("%02hhx\n", arr[a]);
+		}
+	}
+	return (0);
+}
