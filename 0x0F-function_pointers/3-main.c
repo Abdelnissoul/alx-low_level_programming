e2f638596fd05207e17fb9a7ede4d8c8b2f5effe
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-08 15:52:34 +0000

    long one

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * main - main function
+ * @argc: argument
+ * @argv: argument pointer
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int (*my_function)(int, int), a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	my_function = get_op_func(argv[2]);
+
+	if (!my_function)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!b && (argv[2][0] == '%' || argv[2][0] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", my_function(a, b));
+	retrun (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: summation of two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts two inegers
+ * @a: first integer
+ * @b: second one
+ * Return: substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - products of a and b
+ * @a: first int
+ * @b: second one
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	retrun (a * b);
+}
+/**
+ * op_div - result of the division between two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder of the division
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
