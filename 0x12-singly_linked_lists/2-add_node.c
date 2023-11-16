bdd5e8071532c775a09985b937019e8daf33aa46
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-16 14:54:26 +0000

    new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,36 +7,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *n_str;
 	list_t *n_node;
-	list_t length = 0;
+	unsigned int length = 0;
 
-	if (str == NULL)
+	while (str[length])
 	{
-		return (NULL);
+		length = length + 1;
 	}
 
 	n_node = malloc(sizeof(list_t));
-	if (n_node == NULL)
+
+	if (!n_node)
 	{
 		return (NULL);
 	}
 
 	n_node->str = strdup(str);
-	if(n_node->str == NULL)
-	{
-		free(n_node);
-		return (NULL);
-	}
-
-	while (str[length] != '\0')
-	{
-		length = length + 1;
-	}
-
 	n_node->length = length;
-	n_node->next = *head;
-	*head = n_node;
+	n_node->next = (*head);
+	(*head) = n_node;
 
-	return (n_node);
+	return (*head);
 }
