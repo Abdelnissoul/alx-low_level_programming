fdf325b34d67e18ffa6c0a38e0e508c8acb0a9ca
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-21 12:31:14 +0000

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,17 +2,18 @@
 /**
  * print_listint - prints all the elements in the list
  * @h: pointer to the first node
+ *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t node_num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		node_num = node_num + 1;
 		h = h->next;
+		node_num = node_num + 1;
 	}
 	return (node_num);
 }
