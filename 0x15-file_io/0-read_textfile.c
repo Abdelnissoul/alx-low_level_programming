339d2f44154eb6cb33bd7b201ab2e23d070c5a10
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-27 11:13:18 +0000

    read - print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: the name of the file to be read
+ * @letters: number of letters in the file
+ * Return: the number of letters (S), 0 (F)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char buf[READ_BUF_SIZE * 8];
+	ssize_t by;
+
+	if (!filename || !letters)
+	{
+		return (0);
+	}
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		return (0);
+	}
+	 by = read(file, &buf[0], letters);
+	 by = write(STDOUT_FILENO, &buf[0], by);
+
+	 close(file);
+	 return (by);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+0x15. C - File I/O
+By doing these tasks we will be able to know:
+How to create, open, close, read and write files.
+what are file descriptors
+How to use the I/O system calls
+what is the system call
