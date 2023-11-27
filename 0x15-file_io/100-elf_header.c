8710000dd9c9d8d3e46d5a598e6343be87999794
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-27 22:52:25 +0000

    elf header file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -268,7 +268,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	int x;
 	int y;
 
-	o = open(argv[1], O_RDONLY);
+	x = open(argv[1], O_RDONLY);
 	if (x == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
