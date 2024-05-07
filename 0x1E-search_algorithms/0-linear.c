cf619c1593e3ab4be2aad7b7451bcb1ea6dc3fb4
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-07 14:22:04 +0100

    small typo resolve

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,26 +5,25 @@
   * @array: A pointer to the first element of the array.
   * @size: The size of the array.
   * @value: The value (int) to search for.
-  *
   * Return: If the array is NULL -1.
-  *         Otherwise, the first index where the value is located.
+  * Otherwise, the first index where the value is located.
   */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t x;
 
 	if (!array)
-    {
-        return (-1);
-    }
+	{
+		return (-1);
+	}
 
 	for (x = 0; x < size; x++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
 		if (array[x] == value)
-        {
-            return (x);
-        }
+		{
+			return (x);
+		}
 	}
 	return (-1);
 }
